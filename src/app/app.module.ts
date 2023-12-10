02752bd5d7ffb864230e01833a3994d2a0d305ab
eddofr <103290823+eddofr@users.noreply.github.com>
2023-12-10 13:39:11 -0500

    Mi primer commit de mi pagina  de heroes de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
